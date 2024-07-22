d0f216e6660f0957f59b84e6f2b31f7dae33004c
Ariefyusufwinata <winata.arief26@gmail.com>
2024-07-22 11:16:36 +0700

    Initial Commit 🔥

---
+++ android/app/src/main/kotlin/com/riset/moonitoring/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.riset.moonitoring
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
+
+  final messaging = FirebaseMessaging.instance;
+
+  await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+
+  await FirebaseMessaging.instance
+      .subscribeToTopic('notifications-moonitoring');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
