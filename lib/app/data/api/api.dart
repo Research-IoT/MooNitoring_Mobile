0dc896eb4c7e4a7ed74be722f332ee44fde252c3
Ariefyusufwinata <winata.arief26@gmail.com>
2024-08-06 12:01:40 +0700

    update api to development

--- lib/app/data/api/api.dart
+++ lib/app/data/api/api.dart
@@ -4,41 +4,35 @@ class Api {
 
 // ================================================== POST ==================================================
 
-// Auth
   static const register = '/users/register';
   static const login = '/users/login';
 
-// Devices
-  static const registerDevices = '/devices/register';
-  static const sensorUpdate = '/devices/update';
-
-// Device - Sensor
-  static const addData = '/devices_sensor/add';
+  static const changeControl = '/devices/controller/changes';
 
 // ==========================================================================================================
 //
 //
 // ================================================== GET ===================================================
 
-// Auth
-  static const profile = '/profile';
+  static const profile = '/users/profile';
 
-// Devices
-  static const allDevices = '/devices/auth/all';
-  static const sensorInfo = '/devices/sensor';
-  static const detailsDevices = '/devices/details';
+  static const listDevices = '/devices/list';
+  static const listNotif = '/notifications/list';
 
-// Device - Sensor
-  static const getAllData = '/devices_sensor/data';
-  static const getLastData = '/devices_sensor/current';
+  static const dashboard = '/dashboard/info';
+
+  static const infoControl = '/devices/controller/current-users';
+
+  static const dataByDay = '/devices/sensor/data-by-day';
+  static const dataByWeek = '/devices/sensor/data-by-week';
+  static const dataByMonth = '/devices/sensor/data-by-month';
 
 // ==========================================================================================================
 //
 //
 // ================================================= DELETE =================================================
 
-// Auth
-  static const logout = '/logout';
+  static const logout = '/users/logout';
 
 // ==========================================================================================================
 }
